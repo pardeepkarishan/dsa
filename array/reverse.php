e80968587fa6f07408f3be7ef97de3f923c1f37b
pardeepkarishan <47691068+pardeepkarishan@users.noreply.github.com>
2022-10-02 10:54:41 +0530

    --print reverse of given list.

--- array/reverse.php
+++ array/reverse.php
@@ -1,1 +1,14 @@
-<?php?>
+<?php
+
+/**print the reverse of an array
+*/
+function revArray(array $list){
+	$index = sizeof($list)-1;
+	while($index>=0){
+		print_r($list[$index]);
+		$index--;
+	}
+}
+$list = [1,2,3,4];
+revArray($list);
+?>
